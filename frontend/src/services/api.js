56b0186691603155040b9a32dde7e36d2823a55a
adisxsd <nrlhdts403@gmail.com>
2026-08-31 14:53:34 +0700

    modif api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://pitpage-production.up.railway.app/articles', 
+  baseURL: 'https://pitpage-production.up.railway.app/', 
   headers: {
     'Content-Type': 'application/json',
   },
