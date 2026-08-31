b9ab91b0c7cb56e1d115403ebde57883857cb68e
adisxsd <nrlhdts403@gmail.com>
2026-08-31 14:13:29 +0700

    add docker conf

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 const app = require("./app.js");
 
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`server running on port ${PORT}`);
 });
-
